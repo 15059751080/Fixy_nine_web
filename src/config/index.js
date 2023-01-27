0c7989a3732da6260ab2f2c76dd264067080febb
15059751080 <2399601265@qq.com>
2023-01-27 21:06:59 +0800

    first commit

---
+++ src/config/index.js
@@ -0,0 +1,7 @@
+const config = {
+	default_room_bg: 'https://jiangly.com/_nuxt/img/banner_111.a6be22c.gif', // 默认房间背景
+	file_upload_url: 'https://file.jiangly.com/api/upload/file',
+	wx_avatar: 'http://124.71.24.240/myimgs/wx.png'
+};
+
+export default config;
---
+++ src/main.js
@@ -0,0 +1,23 @@
+import ElementUI from 'element-ui';
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import './assets/css/index.less';
+import './socket-io/index.js';
+import './utils/vue.prototype';
+import './icons/index';
+import './permission';
+import './theme/global.less';
+
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
+
+Vue.config.productionTip = false;
+
+new Vue({
+	router,
+	store,
+	render: h => h(App)
+}).$mount('#app');
